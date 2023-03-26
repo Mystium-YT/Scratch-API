142ef4d8b1a478950fd6a730ed49113038aa8901
Mistium <92952823+Mystium-YT@users.noreply.github.com>
2023-03-26 14:13:13 +0100

    Update Scratch-API.js

--- Scratch-API.js
+++ Scratch-API.js
@@ -1,29 +1,32 @@
 class ScratchAPIExt {
-  getInfo() {
+  getInfo () {
     return {
       id: 'scratchapiext',
       name: 'Scratch API',
       blocks: [{
-          opcode: 'gettitle',
-          text: 'get title of scratch project [ID]',
-          blockType: Scratch.BlockType.REPORTER,
-          arguments: {
-            ID: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: 750275219 } }
-        }]
-    };
+        opcode: 'gettitle',
+        text: 'get title of scratch project [ID]',
+        blockType: Scratch.BlockType.REPORTER,
+        arguments: {
+          ID: {
+            type: Scratch.ArgumentType.NUMBER,
+            defaultValue: 750275219
+          }
+        }
+      }]
+    }
   }
+
   gettitle (args) {
     data = fetch('https://trampoline.turbowarp.org/proxy/projects/' + args.URL)
-    return data.title;
+    return data.title
       .then((response) => {
-        return response.text();
+        return response.text()
       })
       .catch((error) => {
-        console.error(error);
-        return 'Uh oh! Something went wrong.';
-      });
+        console.error(error)
+        return 'Uh oh! Something went wrong.'
+      })
   }
 }
-Scratch.extensions.register(new ScratchAPIExt());
+Scratch.extensions.register(new ScratchAPIExt())
